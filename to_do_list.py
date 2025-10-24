14ad61f171ef2a60bcce102484350aa4d73bce3a
ted-M-tech <tetsuya.maeda.mail@gmail.com>
2025-10-23 19:15:03 -0700

    Implement add function

---
+++ to_do_list.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+
+class To_do_list():
+    def __init__(self):
+        pass
+
+    def load_file(self):
+        if os.path.exists('to_do_list.csv'):
+            to_do_list = pd.read_csv('to_do_list.csv')
+        else:
+            to_do_list = pd.DataFrame(columns=['Tasks'])
+            to_do_list.to_csv('to_do_list.csv', index=False)
+        return to_do_list
+
+    def select_menu(self):
+        print("To-do List Application")
+        print("1. Add Task")
+        print("2. Remove Task")
+        print("3. View Tasks")
+        print("4. Exit")
+        selected_menu = input("Enter your choice: ")
+        return selected_menu
+
+    def add_task(self):
+        task = input("Enter the task: ")
+        df_task = pd.DataFrame({'Tasks': [task]})
+        df_task.to_csv('to_do_list.csv', mode='a', header=False, index=False)
+        return print(f"'{task}' has been added to the list.")
+
+    def remove_task(self):
+        pass
+
+    def view_tasks(self):
+        pass
+
+    def exit_app(self):
+        pass
